16234f6d9ebab9fd970a10b06a9fadbc394c0afc
Aya Osama <101758186+oaya2411@users.noreply.github.com>
2023-02-24 13:54:08 +0200

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,14 +1,15 @@
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Board {
     public static int sum = 0;
-    private static int colms;
-    private static int rows;
-    private String[][] board = new String[3][3];
+    /*private static int colms;
+    private static int rows;*/
+    private char[][] board = new char[3][3];
     public void displayBoard(){
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
-                board[i][j] = "-";
+                board[i][j] = '-';
             }
         }
         for(int i = 0; i < 3; i++){
@@ -18,9 +19,23 @@ public class Board {
             System.out.println("|");
         }
     }
+    Scanner cin = new Scanner(System.in);
+    public void updateBoard(int row, int col, char Symbol){
+        if(board[row][col] == '-') {
+            board[row][col] = Symbol;
+        }else{
+            int rr = 0;
+            System.out.println("This position can't be used, please choose empty index");
+            System.out.println("please enter the row: ");
+            rr = cin.nextInt();
+            row = rr;
+            int cc = 0;
+            System.out.println("please enter the column: ");
+            cc = cin.nextInt();
+            col = cc;
+            board[row][col] = Symbol;
 
-    public void updateBoard(int col, int r){
-        board[col][r] = "symbol";
+        }
         System.out.println("The Board Updated To");
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
@@ -29,46 +44,41 @@ public class Board {
             System.out.println("|");
         }
     }
-    public void isWinner(){
-        for(int i = 0; i < 1; i++){//diagonals check
-            for(int j = 0; j < 1; j++){
-                if((Objects.equals(board[i][j], board[i + 1][j + 1]) && Objects.equals(board[i][j], board[i + 2][j + 2]))
-                   && !Objects.equals(board[i][j], "-")){
-                    System.out.println("Found Winner3");
-                    sum += 1;
-                }
-                else if(Objects.equals(board[0][2], board[1][1]) && Objects.equals(board[1][1], board[2][0])
-                        && !Objects.equals(board[0][2], "-")){
-                    System.out.println("Found Winner4");
+    public void isWinner() {
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 1; i++) {
+                if ((Objects.equals(board[i][j], board[i + 1][j]) && Objects.equals(board[i][j], board[i + 2][j])
+                ) && (board[i][j] != '-' && board[i + 1][j] != '-' && board[i + 2][j] != '-')) {//colms
                     sum += 1;
                 }
             }
         }
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 1; j++){
-                if(Objects.equals(board[i][j], board[i][j + 1]) && Objects.equals(board[i][j], board[i][j + 2])
-                        && !Objects.equals(board[i][j], "-")){//rows
-                    System.out.println("Found Winner1");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 1; j++) {
+                if ((Objects.equals(board[i][j], board[i][j + 1]) && Objects.equals(board[i][j], board[i][j + 2]))
+                        && (board[i][j] != '-' && board[i][j + 1] != '-' && board[i][j + 2] != '-')) {//rows
                     sum += 1;
                 }
             }
         }
-        for(int j = 0; j < 3; j++){
-            for(int i = 0; i < 1; i++){
-                if(Objects.equals(board[i][j], board[i + 1][j]) && Objects.equals(board[i][j], board[i + 2][j])
-                        && !Objects.equals(board[i][j], "-")){//colms
-                    System.out.println("Found Winner2");
+
+        for (int i = 0; i < 1; i++) {//diagonals check
+            for (int j = 0; j < 1; j++) {
+                if ((Objects.equals(board[i][j], board[i + 1][j + 1]) && Objects.equals(board[i][j], board[i + 2][j + 2]))
+                        && (board[i][j] != '-')) {
+                    sum += 1;
+                } else if ((Objects.equals(board[0][2], board[1][1]) && Objects.equals(board[1][1], board[2][0])
+                        && (board[0][2] != '-' ))) {
                     sum += 1;
                 }
             }
         }
     }
-
     public void isDraw(){
         if(sum == 0){
-            System.out.println("The Game Finish, You Both Drew");
+            System.out.println("The Game Finished, You Both Drew");
         }
-
     }
 
 }
